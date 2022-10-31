c19c60ea3216a207bea509fa737aaf65d276587e
yudistira65 <yudistiraroseno65@gmail.com>
2022-10-31 23:12:29 +0700

    login selesai

---
+++ page/keranjang.php
@@ -0,0 +1,41 @@
+<div class="card">
+    <div class="card-header">
+        <i class= "fa fa-cart-shopping"></i> 
+         keranjang
+    </div>
+        <div class="card-body">
+           <form action ="" method="post">
+            <table class="table">
+                    <thead>
+                        <tr>
+                            <th colspan="2">komik</th>
+                            <th>jumlah</th>
+                            <th>hapus</th>
+                            <th>total</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>
+                                <img class="img-komik-tbl" src="assets/img/download.jpg" alt="haha">
+                            </td>
+                            <td>
+                                one punch man<br>
+                                Rp. 100,000
+                            </td>
+                            <td>
+                                <button class="btn btn-danger"><i class="fa fa-remove"></i></button>
+                            </td>
+                            <td>Rp. 1,000,000</td>
+                        </tr>
+                    </tbody>
+                    <tfoot>
+                        <tr>
+                            <th colspan="4">total</th>
+                            <th>Rp. 123,000,000</th>
+                        </tr>
+                    </tfoot>
+                </table>
+            </form>
+    </div>
+</div>
--- page/member.php
+++ page/member.php
@@ -1,14 +1,57 @@
+<?php
+    $title = "Daftar";
+    if(isset($_GET['ket'])){
+        $ket = $_GET['ket'];
+        $title = ucfirst($ket);
+        $username = "";
+        $password = "";
+        $no_telpn = "";
+        $level = "";
+
+    }
+?>
 <div class="card">
     <div class="card-header">
-         daftar member
+         <? $title; ?>member
     </div>
     <div class="card-body">
+    <?php
+            if(isset($_GET['ket'])):
+        ?>
+        <form action="modal/p_member.php" method ="POST" >
+            <div class="mb-3">
+                <label for="username" class="form-label">username</label>
+                <input type="text" class="form-control" name="username" id="username" <?= $username!=""?"value='$username'readonly":"";?> reqired>
+            
+            </div>
+            <div class="mb-3">
+                <label for="password" class="form-label">password</label>
+                <input type="text" class="form-control" name="password" id="password"value="<?= $password;?> "reqired >
+            
+            </div>
+            <div class="mb-3">
+                <label for="no_telpn" class="form-label">no_telpn</label>
+                <input type="number" class="form-control" name="no_telpn" id="no_telpn"value="<?=$no_telpn;?>"reqired >
+            
+            </div>
+            <div class="mb-3">
+                <label for="level" class="form-label">level</label>
+                <select name="level" id="level" class= "form-control">
+                    <option value="0">admin</option>
+                    <option value="1">member</option>
+                </select>
+            </div>
+            <input type="submit" name="btnsubmit" value="<?=$title;?>" class="btn btn-primary">
+        </form>
+        <?php else: ?>
+        <a href="index.php?p=member&ket=tambah" class="btn btn-success">Tambah</a>
         <table class="table">
             <thead>
                 <tr>
                     <th>No</th>
                     <th>username</th>
                     <th>no tlpn</th>
+                    <th>level</th>
                 </tr>
             </thead>
             <tbody>
@@ -21,12 +64,14 @@
                 <tr>
                     <td><?= ++$i;?></td>
                     <td><?= $row["username"];?></td>
-                    <td><?= $row["no_tlpn"];?></td>
+                    <td><?= $row["no_telpn"];?></td>
+                    <td><?= $row["level"]==0?"admin":"member";?></td>
                 </tr>
                 <?php
                 endwhile;
                 ?>
             </tbody>
         </table>
+        <?php endif;?>
     </div>
 </div>
